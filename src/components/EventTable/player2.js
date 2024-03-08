55963c7d499fc9d75f244dc4e5dc30c0b4f19296
Swayam Kumar Karn <rohankukarn75@gmail.com>
2024-03-08 08:33:58 +0530

    updated time table

---
+++ src/components/EventTable/player2.js
@@ -0,0 +1,60 @@
+export default [
+    {
+      id: 10,
+      firstName: "SOS Finals",
+      lastName: "9:00am-1:00pm",
+      team: "Civil Dep. Open Area",
+    },
+    {
+      id: 20,
+      firstName: "Code Relay",
+      lastName: "12:30pm to 1:30pm",
+      team: "ET&T Lab",
+      // projects: [
+      //   {
+      //     id: "1",
+      //     name: "test 1"
+      //   },
+      //   {
+      //     id: "2",
+      //     name: "test 2"
+      //   },
+      //   {
+      //     id: "3",
+      //     name: "test 3"
+      //   }
+      // ],
+
+    },
+    {
+      id: 30,
+      firstName: "Quizzify",
+      lastName: "11:00am-1:00pm",
+      team: "G-73 (Mech 4th yr classroom)",
+    },
+    {
+      id: 40,
+      firstName: "Poetry & StoryTelling",
+      lastName: "11:00am-1:00pm",
+      team: "MV Hall",
+    },
+    {
+      id: 50,
+      firstName: "Triple F(FlameLess Food Fiesta)",
+      lastName: "12:00pm-1:00pm",
+      team: "MV Hall",
+    },
+    {
+      id: 60,
+      firstName: "Hunt Warriors",
+      lastName: "2:00pm onwards",
+      team: "MV Hall",
+    },
+    {
+      id: 70,
+      firstName: "Duck & Dodge Ball",
+      lastName: "3:00pm-5:00pm",
+      team: "Parking Ground",
+    },
+  ];
+  
--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import classes from "./Leaderboard.module.css";
-import EventTable from "../components/EventTable/EventTable"
+import EventTable from "../components/EventTable/EventTable";
+import EventTable2 from "../components/EventTable/EventTable2";
+
 
 const Leaderboard = () => {
   return (
@@ -8,12 +10,18 @@ const Leaderboard = () => {
       <div className={classes.leaderboard_section}>
         {/* <h1 className={classes.heading}>Leaderboard</h1> */}
         <div>
-        <h1 className={classes.heading}>Today&apos;s Events</h1>
-
+          <h1 className={classes.heading}>Today&apos;s Events</h1>
         </div>
         {/* <p className={classes.para}>Will be released soon.</p> */}
         <div>
-          <EventTable/>
+          <EventTable2 />
+        </div>
+        <div>
+          <h1 className={classes.heading}>7th March Events</h1>
+        </div>
+        {/* <p className={classes.para}>Will be released soon.</p> */}
+        <div>
+          <EventTable />
         </div>
       </div>
     </>
